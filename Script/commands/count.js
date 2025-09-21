974517e6d334f082462eec3f03f7a479433f2a0e
Rakibrs19x <rakibytr19x@gmail.com>
2025-09-21 18:08:02 +0600

    Update count.js

--- Script/commands/count.js
+++ Script/commands/count.js
@@ -3,7 +3,7 @@ module.exports.config = {
 	name: "count",
 	version: "1.8.0",
 	hasPermssion: 0,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "Rakib-vai_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
 	description: "Check group interactions",
 	commandCategory: "Group",
 	usages: "[all/tag]",
