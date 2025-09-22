425840bf0b713990d6cea400618566002f16ad9b
Rakibrs19x <rakibytr19x@gmail.com>
2025-09-22 07:39:08 +0600

    Update antirobbery.js

--- Script/events/antirobbery.js
+++ Script/events/antirobbery.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "guard",
     eventType: ["log:thread-admins"],
     version: "1.0.0",
-    credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+    credits: "Rakib-vai_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
     description: "Prevent admin changes",
 };
 
